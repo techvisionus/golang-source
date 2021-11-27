788919fc3c38cebfaef159f4ce357237d15e22f9
haminhsang1903 <haminhsang1903@gmail.com>
2021-11-27 22:15:07 +0700

    update route

--- api/routes/v1/product.route.go
+++ api/routes/v1/product.route.go
@@ -4,13 +4,17 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	controllers "github.com/techvisionus/golang-source/api/controllers"
+	controllers "github.com/techvisionus/golang-source/api/controllers/v1"
 )
 
 func addProductRoutes(rg *gin.RouterGroup) {
 	product := rg.Group("/products")
 
-	product.GET("/", func(c *gin.Context) {c.JSON(http.StatusOK, "users")})
-	product.GET("/:id", controllers.GetProductController)
-	product.POST("/", controllers.AddProductController)
+	product.GET("/", func(c *gin.Context) { c.JSON(http.StatusOK, "users") })
+	// product.GET("/:id", controllers.GetProductController)
+	// product.POST("/", controllers.AddProductController)
+	product.GET("/:id", controllers.GetProduct)
+	product.POST("/", controllers.CreateProduct)
+	product.PUT("/", controllers.UpdateProduct)
+	product.DELETE("/:id", controllers.DeleteProduct)
 }
