54c3ae66f8c5d45e1dc5cac3b464301c99e1ff1d
haminhsang1903 <haminhsang1903@gmail.com>
2021-11-27 22:14:17 +0700

    update code for init config pg

--- api/main.go
+++ api/main.go
@@ -2,32 +2,31 @@ package main
 
 import (
 	"net/http"
-	"os"
-
-	database "github.com/techvisionus/golang-source/api/db"
-	"github.com/techvisionus/golang-source/api/routes/v1"
 
 	"github.com/gin-gonic/gin"
+	config "github.com/techvisionus/golang-source/api/config"
+	database "github.com/techvisionus/golang-source/api/db"
+	"github.com/techvisionus/golang-source/api/db/gorm/services"
+	"github.com/techvisionus/golang-source/api/routes/v1"
+	"gorm.io/gorm"
 )
 
 type appError struct {
-    Code     int    `json:"code"`
-    Message  string `json:"message"`
+	Code    int    `json:"code"`
+	Message string `json:"message"`
 }
 
 func main() {
 	router := gin.Default()
 	database.Init()
-
-	router.GET("/", func (context *gin.Context) {
+	// Init connection db
+	services.Initialize(config.POSTGRES, &gorm.Config{})
+	router.GET("/", func(context *gin.Context) {
 		context.String(http.StatusOK, "server is start")
 	})
 
 	v1 := router.Group("/api/v1")
 	routes.GetRoutesV1(v1)
-	port := os.Getenv("PORT")
-	router.Run(":"+ port)
+	port := config.PORT
+	router.Run(":" + port)
 }
-
-
-
